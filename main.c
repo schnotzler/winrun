bc6495a7041d4b2b61a840fbb4e899b26b597604
Leo Schiller <leoschiller1@gmail.com>
2021-08-30 22:28:15 +0200

    Initial commit 	new file:   main.c 	new file:   winrun.cfg

---
+++ main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <inttypes.h>
+
+//run contents of winrun.cfg line by line on command interpreter
+int main(int argc, char *argv[])
+{
+    //create local variables
+    FILE * fp;
+    char * linebuf[ 256+1 ] = { 0 };
+    int8_t retVal = 0;
+
+    //get and chdir executable dir
+    char *dirsep = strrchr( argv[0], '\\' );
+    if ( NULL != dirsep )
+    {
+        *dirsep=0;
+        if ( 0 > chdir( argv[0] ) )
+        {
+            perror( "chdir() not successful" );
+            return -1;
+        }
+    }
+
+    //open cfg file
+    fp = fopen("winrun.cfg", "r");
+    if ( NULL == fp )
+    {
+        perror("Could not open file winrun.cfg");
+        return -2;
+    }
+
+    //read and execute from file line by line
+    while( 0 < fscanf( fp , "%256[A-Za-z0-9 -\\:]\n", &linebuf ) )
+    {
+        printf("> %s\n", linebuf);
+        if ( -1 == system( linebuf ))
+        {
+            perror("ERROR Executing system()");
+            retVal = -3;
+            break;
+        }
+    }
+
+    //close file
+    fclose( fp );
+    return retVal;
+}
